ffc6250652af67f0718f13c447e1293702f16cea
sesamedev <sesamedev@sesamechain.network>
2021-07-07 20:41:38 +0530

    coin name

--- sesame/cmds/units.py
+++ sesame/cmds/units.py
@@ -3,7 +3,7 @@ from typing import Dict
 # The rest of the codebase uses mojos everywhere. Only uses these units
 # for user facing interfaces
 units: Dict[str, int] = {
-    "sesame": 10 ** 12,  # 1 sesame (XCH) is 1,000,000,000,000 mojo (1 Trillion)
+    "sesame": 10 ** 12,  # 1 sesame (XSM) is 1,000,000,000,000 mojo (1 Trillion)
     "mojo:": 1,
     "colouredcoin": 10 ** 3,  # 1 coloured coin is 1000 colouredcoin mojos
 }
